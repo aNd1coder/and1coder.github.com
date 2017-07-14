3dbbbf9075c8fd5d0297faf14a0e9370e106e876
samgui <and1coder@gmail.com>
2017-07-14 14:29:24 +0800

    auto init gitment comments

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,14 @@ $(function() {
         $(this).addClass("prettyprint linenums");
         prettyPrint();
     });
+});
 
+window.onload = function() {
     var gitmentCommentsInitBtn = $('.gitment-comments-init-btn')
     if (gitmentCommentsInitBtn.length > 0) {
         gitmentCommentsInitBtn.trigger('click');
     }
-});
+};
 
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1253455174'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/stat.php%3Fid%3D1253455174' type='text/javascript'%3E%3C/script%3E"));
